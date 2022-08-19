e8d848f69236220ac0566da7315d50c42eb9b1c3
Sravani Sandhya Devi Pyla <sandhyapyla.8@gmail.com>
2022-08-19 22:35:34 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -1,14 +1,25 @@
 const express = require('express')
 const users=require('./Data/user')
+const dotenv=require('dotenv')
+
 const app=express();
+dotenv.config()
 
 app.get('/',(req,res)=>{
  res.send("API is running")
 })
 
-app.get('/api/users',(req,res)=>{
+app.get('/users',(req,res)=>{
   res.json(users)
 })
 
+app.get('/users/:id',(req,res)=>{
+   const user=users.find((n) => n._id == req.params.id);
+   //console.log(req.params)
+   //console.log(user)
+   res.send(user)
+});
 
-app.listen(5000,console.log("server is run at port 5000"))
+const PORT=process.env.PORT||6000
+
+app.listen(PORT,console.log(`server is run at port ${PORT}`))
